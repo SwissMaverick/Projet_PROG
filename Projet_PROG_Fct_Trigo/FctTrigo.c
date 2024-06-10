b9104996f4371b14007b1d85ccd0e75298ffb08c
ETMLNET\jerjeanelie <jeremiego03@gmail.com>
2024-06-10 16:27:16 +0200

    Work in progress...
    Work in progress...

---
+++ Projet_PROG_Fct_Trigo/FctTrigo.c
@@ -0,0 +1,10 @@
+/*  Fonction trigonométrique du projet de PROG  */
+
+trigo()
+{
+	int		degreAngle;
+	float	radiantAngle;
+
+	float abscisse, oppose, hypothenuse;
+	float sinus, cosinus, tangente;
+}
